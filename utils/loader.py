f28d39fd05f310b0f7923f025e45999bcfe42c60
Daniel Andrew <danielandrewtt@gmail.com>
2023-07-17 15:00:57 +0800

    feat(): tested loader and process

--- utils/loader.py
+++ utils/loader.py
@@ -3,12 +3,18 @@
 
 import os
 import sys
+import argparse
 import numpy as np
 import pandas as pd
 
+'''
+USAGE: 
+python loader.py [--p PREPROCESSOR] [--fr FRAC] [--n NOVALRSS] [--f FLOOR]
+'''
+
 class Loader(object):
     def __init__(self,
-                 path='../datas/dataset.csv',
+                 path='../datas/',
                  frac=0.1,
                  preprocessor='standard_scaler',
                  prefix='IPS-LOADER',
@@ -46,10 +52,11 @@ class Loader(object):
         self.testing_data = None
         self.testing_df = None
         self.load_data() # Load the Data
+        self.process_data()
 
     def load_data(self):
-        self.training_df = pd.read_csv(self.training_fname, header=0, frac=self.frac)
-        self.testing_df = pd.read_csv(self.testing_fname, header=0, frac=self.frac)
+        self.training_df = pd.read_csv(self.training_fname, header=0)
+        self.testing_df = pd.read_csv(self.testing_fname, header=0)
 
         self.training_df = self.training_df[self.training_df['floor'] == self.floor]
         self.testing_df = self.testing_df[self.testing_df['floor'] == self.floor]
@@ -61,23 +68,19 @@ class Loader(object):
             self.training_df = self.training_df.sample(frac=self.frac)
             self.testing_df = self.testing_df.sample(frac=self.frac)
         
-        print('Training Data Loaded: ')
-        print(self.training_df)
-        print('Testing Data Loaded: ')
-        print(self.testing_df)
+        # print('Training Data Loaded: ')
+        # print(self.training_df['floor'])
+
+        # print('Testing Data Loaded: ')
+        # print(self.testing_df['floor'])
 
     def process_data(self):
-        
-        # Get min and max rss values of training dan testing data
-        rss_min_train, rss_max_train = self.get_min_max_rss(self.training_df)
-        rss_min_train, rss_max_test = self.get_min_max_rss(self.testing_df)
-
         # Fill missing values rssi values with 100
         no_waps = self.no_waps
         self.training_df[no_waps] = self.training_df[no_waps].fillna(self.no_val_rss)
         self.testing_df[no_waps] = self.testing_df[no_waps].fillna(self.no_val_rss)
 
-        rss_training = np.assarray(self.training_df[no_waps])
+        rss_training = np.asarray(self.training_df[no_waps])
         rss_testing = np.asarray(self.testing_df[no_waps])
         
         # Scale the flattened rssi data
@@ -91,12 +94,12 @@ class Loader(object):
             rss_testing_scaled = rss_testing
         
         # Process Coords
-        training_coord_x = np.asarray(self.training_df['LONGITUDE'], dtype=float)
-        training_coord_y = np.asarray(self.training_df['LATITUDE'], dtype=float)
+        training_coord_x = np.asarray(self.training_df['xr'], dtype=float)
+        training_coord_y = np.asarray(self.training_df['yr'], dtype=float)
         training_coords = np.column_stack((training_coord_x, training_coord_y))
 
-        testing_coord_x = np.asarray(self.testing_df['LONGITUDE'], dtype=float)
-        testing_coord_y = np.asarray(self.testing_df['LATITUDE'], dtype=float)
+        testing_coord_x = np.asarray(self.testing_df['xr'], dtype=float)
+        testing_coord_y = np.asarray(self.testing_df['yr'], dtype=float)
         testing_coords = np.column_stack((testing_coord_x, testing_coord_y))
 
         # Scale the stacked coords data
@@ -107,11 +110,68 @@ class Loader(object):
             training_coords_scaled = training_coords
             training_coords_scaled = testing_coords
         
+        # print('rssi data')
+        # print(rss_training)
+        # print(rss_testing)
+        # print('\n')
+        # print('Scaled rssi data')
+        # print(rss_training_scaled)
+        # print(rss_testing_scaled)
+        # print('\n')
+        # print('coords data')
+        # print(training_coords)
+        # print(testing_coords)
+        # print('scaled coords data')
+        # print(training_coords_scaled)
+        # print(testing_coords_scaled)
+        
     def save_data(self):
         pass
 
-    def get_min_max_rss(self, data):
-        min_val = data[self.no_waps].min().min()
-        max_val = data[self.no_waps].max().max()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
-        return min_val, max_val
+    parser.add_argument(
+        '--p',
+        '--preprocessor',
+        help='preprocessor method',
+        dest='preprocessor',
+        default='standard_scaler',
+        type=str,
+    )
+    parser.add_argument(
+        '--fr',
+        '--frac',
+        help='fraction of data to be sampled',
+        dest='frac',
+        default=1.0,
+        type=float
+    )
+    parser.add_argument(
+        '--n',
+        '--novalrss',
+        help='no rss value indicator',
+        dest='novalrss',
+        default=-100,
+        type=int
+    )
+    parser.add_argument(
+        '--f',
+        '--floor',
+        help='Indicates which floor to load',
+        dest='floor',
+        default=1,
+        type=int
+    )
+    args = parser.parse_args()
+    preprocessor = args.preprocessor
+    frac = args.frac
+    novalrss = args.novalrss
+    floor = args.floor
+
+    dataset = Loader(
+        preprocessor=preprocessor,
+        frac=frac,
+        no_val_rss=novalrss,
+        floor=floor
+    )
